8691461b726d0aaab73c5429c2035f4f19d1add6
benben-miao <benben.miao@outlook.com>
2025-06-20 16:18:16 +0800

    vite.config.js base'

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // bash: "./",
+  base: "/BenthicGermplasm/",
   plugins: [vue()],
   resolve: {
     alias: {
